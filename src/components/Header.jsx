3620bc436e8896f66aa7e37d14de518208407ee9
cerbin1 <gekarez@gmail.com>
2024-07-12 17:19:55 +0200

    Add navbar with some styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,17 @@ export default function Header() {
     <header>
       <div>Afterady</div>
 
-      <nav>Navigation</nav>
+      <nav>
+        <a href="#" className="nav-item">
+          Kategorie
+        </a>
+        <a href="#" className="nav-item">
+          Ranking
+        </a>
+        <a href="#" className="nav-item">
+          Zaproponuj
+        </a>
+      </nav>
       <span>Search</span>
     </header>
   );
--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -8,3 +8,14 @@ test("should get logo", () => {
   const logo = screen.getByText("Afterady");
   expect(logo).toBeInTheDocument(true);
 });
+
+test("should get three navbar items", () => {
+  render(<Header />);
+
+  const navbar = screen.getByRole("navigation");
+  expect(navbar).toBeInTheDocument(true);
+  expect(navbar.children.length).toBe(3);
+  expect(navbar.children[0]).toHaveTextContent("Kategorie");
+  expect(navbar.children[1]).toHaveTextContent("Ranking");
+  expect(navbar.children[2]).toHaveTextContent("Zaproponuj");
+});
