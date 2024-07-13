506f7cd2ae863489fe50a10e5c291c8df5213a46
cerbin1 <gekarez@gmail.com>
2024-07-13 07:14:58 +0200

    Add describe to Header.test.js

--- src/components/header/Header.test.js
+++ src/components/header/Header.test.js
@@ -2,20 +2,22 @@ import { render, screen } from "@testing-library/react";
 import Header from "./Header";
 import "@testing-library/jest-dom";
 
-test("should get logo", () => {
-  render(<Header />);
+describe("Header", () => {
+  test("should get logo", () => {
+    render(<Header />);
 
-  const logo = screen.getByText("Afterady");
-  expect(logo).toBeInTheDocument(true);
-});
+    const logo = screen.getByText("Afterady");
+    expect(logo).toBeInTheDocument(true);
+  });
 
-test("should get three navbar items", () => {
-  render(<Header />);
+  test("should get three navbar items", () => {
+    render(<Header />);
 
-  const navbar = screen.getByRole("navigation");
-  expect(navbar).toBeInTheDocument(true);
-  expect(navbar.children.length).toBe(3);
-  expect(navbar.children[0]).toHaveTextContent("Kategorie");
-  expect(navbar.children[1]).toHaveTextContent("Ranking");
-  expect(navbar.children[2]).toHaveTextContent("Zaproponuj");
+    const navbar = screen.getByRole("navigation");
+    expect(navbar).toBeInTheDocument(true);
+    expect(navbar.children.length).toBe(3);
+    expect(navbar.children[0]).toHaveTextContent("Kategorie");
+    expect(navbar.children[1]).toHaveTextContent("Ranking");
+    expect(navbar.children[2]).toHaveTextContent("Zaproponuj");
+  });
 });
