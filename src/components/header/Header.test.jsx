46d98b554f703f3e3c9eb29a940f5d301805d103
cerbin1 <gekarez@gmail.com>
2024-07-13 07:40:18 +0200

    Add vitest config and update tests to it to have tests run update on changing code

--- src/components/header/Header.test.jsx
+++ src/components/header/Header.test.jsx
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react";
 import Header from "./Header";
-import "@testing-library/jest-dom";
 
 describe("Header", () => {
   test("should get logo", () => {
--- src/components/home/Home.test.jsx
+++ src/components/home/Home.test.jsx
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react";
 import Home from "./Home";
-import "@testing-library/jest-dom";
 
 describe("Home", () => {
   test("should display hero section", () => {
