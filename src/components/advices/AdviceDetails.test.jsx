d775ceca493d88501f83f3286aae2b63aa8f4d5d
cerbin1 <gekarez@gmail.com>
2024-07-13 11:54:57 +0200

    Improve test name

--- src/components/advices/AdviceDetails.test.jsx
+++ src/components/advices/AdviceDetails.test.jsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import AdviceDetails from "./AdviceDetails";
 
 describe("AdviceDetails", () => {
-  test("should display name", () => {
+  test("should display advice name and content", () => {
     render(<AdviceDetails />);
 
     expect(screen.getByText("Nazwa porady")).toBeInTheDocument();
