43167fcddc0116c27ba398e801dfe159da265195
cerbin1 <gekarez@gmail.com>
2024-07-15 11:55:22 +0200

    Improve Categories component - add more categories, add description, add css

--- src/components/advices/categories/Categories.jsx
+++ src/components/advices/categories/Categories.jsx
@@ -5,32 +5,44 @@ const categories = [
     id: 1,
     advicesCount: 15,
     name: "Rozwój osobisty",
+    description: "Porady dotyczące samorozwoju i motywacji.",
   },
   {
     id: 2,
     advicesCount: 102,
     name: "Dom",
+    description:
+      "Porady dotyczące sprzątania, zarządzania przestrzenią itp. w domu.",
   },
   {
     id: 3,
     advicesCount: 52,
-    name: "Praca",
+    name: "Zdrowie",
+    description: "Porady dotyczące zdrowia i dobrego samopoczucia.",
   },
   {
     id: 4,
+    advicesCount: 52,
+    name: "Finanse",
+    description: "Porady dotyczące zarządzania finansami.",
+  },
+  {
+    id: 5,
     advicesCount: 0,
-    name: "Zwierzęta",
+    name: "Technologia",
+    description: "Porady dotyczące nowinek technologicznych.",
   },
 ];
 
 export default function Categories() {
   return (
-    <div>
-      <h2>Kategorie</h2>
-      <table>
+    <section className="container" data-testid="categories-section">
+      <h1 className="categories-title">Kategorie Porad</h1>
+      <table className="categories-table">
         <thead>
           <tr>
             <th>Kategoria</th>
+            <th>Opis</th>
             <th>Liczba porad</th>
             <th>Szczegóły</th>
           </tr>
@@ -39,6 +51,7 @@ export default function Categories() {
           {categories.map((category) => (
             <tr key={category.id}>
               <td>{category.name}</td>
+              <td>{category.description}</td>
               <td>{category.advicesCount}</td>
               <td>
                 <Link to={"/categories/" + category.id}>
@@ -49,6 +62,6 @@ export default function Categories() {
           ))}
         </tbody>
       </table>
-    </div>
+    </section>
   );
 }
--- src/components/advices/categories/Categories.test.jsx
+++ src/components/advices/categories/Categories.test.jsx
@@ -1,13 +1,21 @@
 import { screen } from "@testing-library/react";
 import Categories from "./Categories";
 import { renderWithRouter } from "../../../test-utils";
+import { expect } from "vitest";
 
 describe("Categories", () => {
   test("should display categories", () => {
     renderWithRouter(<Categories />);
 
-    expect(screen.getByText("Kategorie")).toBeInTheDocument();
-    expect(screen.getByRole("table")).toBeInTheDocument();
-    expect(screen.queryAllByRole("row")).toHaveLength(5);
+    const section = screen.getByTestId("categories-section");
+    expect(section).toBeInTheDocument();
+    expect(section).toHaveClass("container");
+    expect(screen.getByText("Kategorie Porad")).toBeInTheDocument();
+    const table = screen.getByRole("table");
+    expect(table).toBeInTheDocument();
+    expect(table).toHaveClass("categories-table");
+    expect(screen.getAllByRole("rowgroup")).toHaveLength(2);
+    expect(screen.getAllByRole("row")).toHaveLength(6);
+    expect(screen.getAllByRole("cell")).toHaveLength(20);
   });
 });
