d67fb6ad1f3274ab833ed81afc99b9923cb45b61
cerbin1 <gekarez@gmail.com>
2024-07-16 10:05:27 +0200

    Add tailwind to SuggestAdvice component

--- src/components/advices/SuggestAdvice.jsx
+++ src/components/advices/SuggestAdvice.jsx
@@ -1,3 +1,6 @@
+import ContainerSection from "../common/ContainerSection";
+import Button from "../common/Button";
+
 const categories = [
   { id: 0, name: "Wybierz kategorię" },
   { id: 1, name: "Rozwój osobisty" },
@@ -8,25 +11,47 @@ const categories = [
 
 export default function SuggestAdvice() {
   return (
-    <>
+    <ContainerSection data-testid="suggest-advice-section">
       <h1>Zaproponuj poradę</h1>
-      <form aria-label="SuggestAdvice">
-        <label htmlFor="name">Nazwa porady</label>
-        <input id="name" name="name" type="text" />
+      <form
+        aria-label="SuggestAdvice"
+        className="flex flex-col gap-4 text-lg w-1/3"
+      >
+        <div className="flex flex-col gap-2 pt-4 border-t border-r border-l border-slate-200">
+          <label htmlFor="name">Nazwa porady</label>
+          <input
+            id="name"
+            name="name"
+            type="text"
+            className="bg-slate-200 rounded py-2 px-1"
+          />
+        </div>
 
-        <label htmlFor="name">Kategoria</label>
-        <select name="category" id="category">
-          {categories.map((category) => (
-            <option key={category.id} value={category.id}>
-              {category.name}
-            </option>
-          ))}
-        </select>
+        <div className="flex flex-col gap-2 pt-4 border-t border-r border-l border-slate-200">
+          <label htmlFor="category">Kategoria</label>
+          <select
+            name="category"
+            id="category"
+            className="bg-slate-200 rounded py-2"
+          >
+            {categories.map((category) => (
+              <option key={category.id} value={category.id}>
+                {category.name}
+              </option>
+            ))}
+          </select>
+        </div>
 
-        <label htmlFor="content">Treść</label>
-        <textarea name="content" id="content"></textarea>
-        <button>Wyślij propozycję</button>
+        <div className="flex flex-col gap-2 pt-4 border-t border-r border-l border-slate-200">
+          <label htmlFor="content">Treść</label>
+          <textarea
+            name="content"
+            id="content"
+            className="bg-slate-200 rounded py-2 px-1"
+          ></textarea>
+        </div>
+        <Button>Wyślij propozycję</Button>
       </form>
-    </>
+    </ContainerSection>
   );
 }
--- src/components/advices/SuggestAdvice.test.jsx
+++ src/components/advices/SuggestAdvice.test.jsx
@@ -5,8 +5,11 @@ describe("SuggestAdvice", () => {
   it("should display form", () => {
     render(<SuggestAdvice />);
 
+    expect(screen.getByTestId("suggest-advice-section")).toBeInTheDocument();
     expect(screen.getByText("Zaproponuj poradę")).toBeInTheDocument();
-    expect(screen.getByRole("form")).toBeInTheDocument();
+    const form = screen.getByRole("form");
+    expect(form).toBeInTheDocument();
+    expect(form).toHaveClass("flex flex-col gap-4");
     expect(screen.getByText("Nazwa porady")).toBeInTheDocument();
     expect(screen.getByText("Kategoria")).toBeInTheDocument();
     expect(screen.getByText("Treść")).toBeInTheDocument();
