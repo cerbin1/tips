0a7585f713606ff4167e64082708d9e2fdd64ec1
cerbin1 <gekarez@gmail.com>
2024-07-16 10:57:22 +0200

    Init backend

---
+++ backend/src/test/java/afterady/AfteradyApplicationTests.java
@@ -0,0 +1,13 @@
+package afterady;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfteradyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
