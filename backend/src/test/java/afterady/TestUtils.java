361b155734122ebd9bdebe9b4d06ee8303e2b472
cerbin1 <gekarez@gmail.com>
2024-07-17 23:15:31 +0200

    Remove @UtilityClass and change Utils class to private final with final static methods - problems with ide

--- backend/src/test/java/afterady/TestUtils.java
+++ backend/src/test/java/afterady/TestUtils.java
@@ -1,14 +1,14 @@
 package afterady;
 
 import afterady.domain.user.User;
-import lombok.experimental.UtilityClass;
 
 import static java.util.Collections.emptySet;
 
-@UtilityClass
-public class TestUtils {
+public final class TestUtils {
+    private TestUtils() {
+    }
 
-    public User testUser() {
+    public static User testUser() {
         return new User(1L, "username", "email", "password", false, emptySet());
     }
 }
