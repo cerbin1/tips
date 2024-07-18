31d1e11932b744132019c0844803e0c22a7e9603
cerbin1 <gekarez@gmail.com>
2024-07-18 17:25:33 +0200

    Fix NavItem component - use a element instead of Link from react router

--- src/components/header/NavItem.jsx
+++ src/components/header/NavItem.jsx
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
+
 export default function NavItem({ href, children }) {
   return (
-    <a className="no-underline px-12 text-blue-to-light" href={href}>
+    <Link className="no-underline px-12 text-blue-to-light" to={href}>
       {children}
-    </a>
+    </Link>
   );
 }
