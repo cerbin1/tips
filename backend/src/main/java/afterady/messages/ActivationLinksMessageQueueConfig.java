ff2ecf0fe16353466eeebb5ac28305e5edd3099b
cerbin1 <gekarez@gmail.com>
2024-07-18 15:17:00 +0200

    Fix rabbitmq - add json message converter configuration

--- backend/src/main/java/afterady/messages/ActivationLinksMessageQueueConfig.java
+++ backend/src/main/java/afterady/messages/ActivationLinksMessageQueueConfig.java
@@ -7,6 +7,9 @@ import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.DefaultJackson2JavaTypeMapper;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -34,6 +37,21 @@ public class ActivationLinksMessageQueueConfig {
 
     @Bean
     public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
-        return new RabbitTemplate(connectionFactory);
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
+    }
+
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        Jackson2JsonMessageConverter converter = new Jackson2JsonMessageConverter();
+        converter.setClassMapper(new DefaultJackson2JavaTypeMapper() {
+            @Override
+            public void setTrustedPackages(String... trustedPackages) {
+                super.setTrustedPackages("afterady");
+            }
+        });
+        return converter;
     }
 }
