b7c6201a619e59a25b5e4f686ce08201aa201ce7
cerbin1 <gekarez@gmail.com>
2024-07-22 17:36:19 +0200

    Improve user registration - implement front for resend link

---
+++ src/components/user/RegisterUser.jsx
@@ -0,0 +1,171 @@
+import Button from "../common/Button";
+import ContainerSection from "../common/ContainerSection";
+import FormInput from "../common/FormInput";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function RegisterUser() {
+  const [passwordsAreNotEqual, setPasswordsAreNotEqual] = useState(false);
+  const [userCreateLoading, setUserCreateLoading] = useState(false);
+  const [userCreateError, setUserCreateError] = useState(undefined);
+  const [resendLinkLoading, setResendLinkLoading] = useState(false);
+  const [resendLinkSent, setResendLinkSent] = useState(false);
+  const [resendLinkError, setResendLinkError] = useState(undefined);
+  const [activationLink, setActivationLink] = useState();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    async function sendRequest() {
+      setUserCreateLoading(true);
+      setPasswordsAreNotEqual(false);
+      setUserCreateError(undefined);
+      const formData = new FormData(event.target);
+      const email = formData.get("email");
+      const username = formData.get("username");
+      const password = formData.get("password");
+      const passwordRepeat = formData.get("password-repeat");
+
+      if (password !== passwordRepeat) {
+        setPasswordsAreNotEqual(true);
+        setUserCreateLoading(false);
+        return;
+      }
+      const url = import.meta.env.VITE_BACKEND_URL + "auth/register";
+      const userData = { email, username, password, passwordRepeat };
+      try {
+        const response = await fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(userData),
+        });
+        if (response.ok) {
+          const link = await response.json();
+          setActivationLink(link);
+        } else {
+          if (response.status === 422) {
+            const error = await response.json();
+            if (error.message === "Error: Username is already taken.") {
+              setUserCreateError("Nazwa użytkownika jest zajęta!");
+            } else if (error.message === "Error: Email is already in use.") {
+              setUserCreateError("Email jest zajęty!");
+            }
+          } else {
+            throw new Error(userCreateError.message);
+          }
+        }
+      } catch (error) {
+        setUserCreateError("Nie udało się utworzyć użytkownika!");
+      }
+
+      setUserCreateLoading(false);
+    }
+    sendRequest();
+  }
+
+  async function resendLink() {
+    setResendLinkLoading(true);
+    setResendLinkError(undefined);
+    try {
+      const url = import.meta.env.VITE_BACKEND_URL + "auth/resend/";
+      const response = await fetch(url + activationLink, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.ok) {
+        setResendLinkSent(true);
+      } else {
+        throw new Error();
+      }
+    } catch (error) {
+      setResendLinkError("Nie udało się ponownie wysłać linka!");
+    }
+    setResendLinkLoading(false);
+  }
+
+  return (
+    <ContainerSection data-testid="register-user-section">
+      <h1>Rejestracja</h1>
+      {!activationLink && (
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col gap-4 w-1/3"
+          aria-label="RegisterUser"
+        >
+          <FormInput label="Adres e-mail" id="email" type="email" required />
+          <FormInput label="Nazwa użytkownika" id="username" required />
+          <FormInput
+            label="Hasło"
+            id="password"
+            type="password"
+            minLength={8}
+            required
+          />
+          <FormInput
+            label="Powtórz hasło"
+            id="password-repeat"
+            type="password"
+            minLength={8}
+            required
+          />
+          {passwordsAreNotEqual && (
+            <p className="text-red-500">Hasła muszą się zgadzać!</p>
+          )}
+          <div className="flex justify-between">
+            <button
+              className="px-6 py-3 bg-slate-400 text-white text-lg rounded hover:bg-slate-500 transition-colors duration-300"
+              type="reset"
+              disabled={userCreateLoading}
+            >
+              Wyczyść formularz
+            </button>
+            <Button type="submit" disabled={userCreateLoading}>
+              {userCreateLoading ? "Wysyłanie..." : "Wyślij"}
+            </Button>
+          </div>
+        </form>
+      )}
+      {!activationLink && userCreateError && (
+        <div className="py-6 text-red-500">{userCreateError}</div>
+      )}
+
+      {resendLinkError && (
+        <div className="py-6 text-red-500">{resendLinkError}</div>
+      )}
+
+      {activationLink && !resendLinkError && (
+        <>
+          <div className="my-4 py-2 px-8 border border-cyan-200 radius-2xl text-green-600 leading-loose">
+            {resendLinkSent && <p>Link został wysłany ponownie.</p>}
+
+            {!resendLinkSent && (
+              <>
+                <p>Rejestracja przebiegła pomyślnie.</p>
+                <p>Na Twój adres e-mail został wysłany link aktywacyjny.</p>
+                <p>Link wygaśnie po 15 minutach.</p>
+                <p>
+                  <button
+                    onClick={resendLink}
+                    className="text-blue-to-dark underline"
+                    disabled={resendLinkLoading}
+                  >
+                    Kliknij tutaj
+                  </button>{" "}
+                  jeśli chcesz wysłać ponownie link.
+                </p>
+              </>
+            )}
+          </div>
+          <Button>
+            <Link to="/login">Przejdź do logowania</Link>
+          </Button>
+        </>
+      )}
+    </ContainerSection>
+  );
+}
--- src/components/user/RegisterUser.test.jsx
+++ src/components/user/RegisterUser.test.jsx
@@ -1,7 +1,9 @@
 import { fireEvent, render, screen } from "@testing-library/react";
-import RegisterUser from "../../../RegisterUser";
 import userEvent from "@testing-library/user-event";
 import { beforeAll, beforeEach, expect, vi } from "vitest";
+import { renderWithRouter } from "../../test-utils";
+import RegisterUser from "./RegisterUser";
+// import { renderWithRouter } from "../../test-utils";
 
 const mockedUseNavigate = vi.fn();
 
@@ -141,18 +143,7 @@ describe("RegisterUser", () => {
 
   test("should display general error when response is not ok", async () => {
     render(<RegisterUser />);
-    fireEvent.change(screen.getByLabelText("Adres e-mail"), {
-      target: { value: "test@email" },
-    });
-    fireEvent.change(screen.getByLabelText("Nazwa użytkownika"), {
-      target: { value: "username" },
-    });
-    fireEvent.change(screen.getByLabelText("Hasło"), {
-      target: { value: "password" },
-    });
-    fireEvent.change(screen.getByLabelText("Powtórz hasło"), {
-      target: { value: "password" },
-    });
+    fillForm();
 
     const submitButton = screen.getByText("Wyślij");
     await userEvent.click(submitButton);
@@ -165,18 +156,7 @@ describe("RegisterUser", () => {
 
   test("should display error when username is already taken", async () => {
     render(<RegisterUser />);
-    fireEvent.change(screen.getByLabelText("Adres e-mail"), {
-      target: { value: "test@email" },
-    });
-    fireEvent.change(screen.getByLabelText("Nazwa użytkownika"), {
-      target: { value: "username" },
-    });
-    fireEvent.change(screen.getByLabelText("Hasło"), {
-      target: { value: "password" },
-    });
-    fireEvent.change(screen.getByLabelText("Powtórz hasło"), {
-      target: { value: "password" },
-    });
+    fillForm();
     globalThis.fetch = vi.fn(() =>
       Promise.resolve({
         ok: false,
@@ -194,10 +174,122 @@ describe("RegisterUser", () => {
     expect(error).toBeInTheDocument();
     expect(error).toHaveClass("py-6 text-red-500");
   });
+
+  test("should display error when email is already taken", async () => {
+    render(<RegisterUser />);
+    fillForm();
+    globalThis.fetch = vi.fn(() =>
+      Promise.resolve({
+        ok: false,
+        status: 422,
+        json: () =>
+          Promise.resolve({ message: "Error: Email is already in use." }),
+      })
+    );
+
+    const submitButton = screen.getByText("Wyślij");
+    await userEvent.click(submitButton);
+
+    expect(globalThis.fetch).toHaveBeenCalledOnce();
+    const error = screen.getByText("Email jest zajęty!");
+    expect(error).toBeInTheDocument();
+    expect(error).toHaveClass("py-6 text-red-500");
+  });
+
+  test("should send form successfully", async () => {
+    renderWithRouter(<RegisterUser />);
+    expect(screen.getByRole("form")).toBeInTheDocument();
+
+    await fillAndSubmitForm();
+
+    expect(globalThis.fetch).toHaveBeenCalledWith(
+      "http://localhost:8080/auth/register",
+      {
+        body: '{"email":"test@email","username":"username","password":"password","passwordRepeat":"password"}',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+      }
+    );
+    expect(screen.queryByRole("form")).toBeNull();
+    expect(screen.getAllByRole("generic").at(2)).toHaveClass(
+      "my-4 py-2 px-8 border border-cyan-200 radius-2xl text-green-600 leading-loose"
+    );
+    expect(
+      screen.getByText("Rejestracja przebiegła pomyślnie.")
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText("Na Twój adres e-mail został wysłany link aktywacyjny.")
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText("Link wygaśnie po 15 minutach.")
+    ).toBeInTheDocument();
+    const button = screen.getByText("Kliknij tutaj");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveRole("button");
+    expect(button).toHaveClass("text-blue-to-dark underline");
+    expect(
+      screen.getByText("jeśli chcesz wysłać ponownie link.")
+    ).toBeInTheDocument();
+    const loginButton = screen.getByText("Przejdź do logowania");
+    expect(loginButton).toBeInTheDocument();
+    expect(loginButton).toHaveAttribute("href", "/login");
+  });
+
+  test("should display error and hide resend button when resend activation link failed", async () => {
+    renderWithRouter(<RegisterUser />);
+    await fillAndSubmitForm();
+    expect(globalThis.fetch).toHaveBeenCalledOnce();
+    const resendActivationLinkButton = screen.getByText("Kliknij tutaj");
+    expect(resendActivationLinkButton).toBeInTheDocument();
+
+    globalThis.fetch = vi.fn(() => Promise.resolve({ ok: false }));
+    await userEvent.click(resendActivationLinkButton);
+
+    expect(globalThis.fetch).toHaveBeenCalledWith(
+      "http://localhost:8080/auth/resend/63b4072b-b8c8-4f9a-acf4-76d0948adc6e",
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+      }
+    );
+    const error = screen.getByText("Nie udało się ponownie wysłać linka!");
+    expect(error).toBeInTheDocument();
+    expect(error).toHaveClass("py-6 text-red-500");
+    expect(screen.queryByText("Kliknij tutaj")).toBeNull();
+  });
+
+  test("should successfully resend activation link and then display info and hide resend button", async () => {
+    renderWithRouter(<RegisterUser />);
+    await fillAndSubmitForm();
+    expect(globalThis.fetch).toHaveBeenCalledOnce();
+    const resendActivationLinkButton = screen.getByText("Kliknij tutaj");
+    expect(resendActivationLinkButton).toBeInTheDocument();
+
+    globalThis.fetch = vi.fn(() => Promise.resolve({ ok: true }));
+    await userEvent.click(resendActivationLinkButton);
+
+    expect(globalThis.fetch).toHaveBeenCalledWith(
+      "http://localhost:8080/auth/resend/63b4072b-b8c8-4f9a-acf4-76d0948adc6e",
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+      }
+    );
+    expect(
+      screen.getByText("Link został wysłany ponownie.")
+    ).toBeInTheDocument();
+    expect(screen.getByText("Przejdź do logowania")).toBeInTheDocument();
+    expect(screen.queryByText("Kliknij tutaj")).toBeNull();
+  });
 });
 
-test("should display error when email is already taken", async () => {
-  render(<RegisterUser />);
+function fillForm() {
   fireEvent.change(screen.getByLabelText("Adres e-mail"), {
     target: { value: "test@email" },
   });
@@ -210,43 +302,16 @@ test("should display error when email is already taken", async () => {
   fireEvent.change(screen.getByLabelText("Powtórz hasło"), {
     target: { value: "password" },
   });
+}
+
+async function fillAndSubmitForm() {
+  fillForm();
   globalThis.fetch = vi.fn(() =>
     Promise.resolve({
-      ok: false,
-      status: 422,
-      json: () =>
-        Promise.resolve({ message: "Error: Email is already in use." }),
+      ok: true,
+      status: 200,
+      json: () => Promise.resolve("63b4072b-b8c8-4f9a-acf4-76d0948adc6e"),
     })
   );
-
-  const submitButton = screen.getByText("Wyślij");
-  await userEvent.click(submitButton);
-
-  expect(globalThis.fetch).toHaveBeenCalledOnce();
-  const error = screen.getByText("Email jest zajęty!");
-  expect(error).toBeInTheDocument();
-  expect(error).toHaveClass("py-6 text-red-500");
-});
-
-test("should send form successfully", async () => {
-  render(<RegisterUser />);
-  fireEvent.change(screen.getByLabelText("Adres e-mail"), {
-    target: { value: "test@email" },
-  });
-  fireEvent.change(screen.getByLabelText("Nazwa użytkownika"), {
-    target: { value: "username" },
-  });
-  fireEvent.change(screen.getByLabelText("Hasło"), {
-    target: { value: "password" },
-  });
-  fireEvent.change(screen.getByLabelText("Powtórz hasło"), {
-    target: { value: "password" },
-  });
-  globalThis.fetch = vi.fn(() => Promise.resolve({ ok: true }));
-
-  const submitButton = screen.getByText("Wyślij");
-  await userEvent.click(submitButton);
-
-  expect(globalThis.fetch).toHaveBeenCalledOnce();
-  expect(mockedUseNavigate).toHaveBeenCalledOnce();
-});
+  await userEvent.click(screen.getByText("Wyślij"));
+}
