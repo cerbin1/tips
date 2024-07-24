b78035230b094d001559ae1eae54af04c6a986be
cerbin1 <gekarez@gmail.com>
2024-07-24 13:33:24 +0200

    Fix login - add useEffect with token loading in RootLayout to rerender component when token changes value in localStorage

--- src/router/RootLayout.jsx
+++ src/router/RootLayout.jsx
@@ -1,7 +1,16 @@
-import { Outlet } from "react-router";
+import { Outlet, useLoaderData } from "react-router";
 import Header from "../components/header/Header";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  const token = useLoaderData();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+  }, [token]);
+
   return (
     <>
       <Header />
