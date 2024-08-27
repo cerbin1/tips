dd4624b17885e34d89cdbb40355b77b82a0864ed
cerbin1 <gekarez@gmail.com>
2024-08-27 14:23:22 +0200

    Add missing test-utils export

--- src/test-utils.js
+++ src/test-utils.js
@@ -1,8 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
 import { render } from "@testing-library/react";
+import AuthProvider from "./store/auth-context";
 
 export const renderWithRouter = (ui) => {
   return {
     ...render(ui, { wrapper: BrowserRouter }),
+  };
+};
+
+export const renderWithAuthProvider = (ui) => {
+  return {
+    ...render(ui, { wrapper: AuthProvider }),
   };
 };
