062dc23267ced108b3fb11b222c284ea2d4f47ab
cerbin1 <gekarez@gmail.com>
2024-08-29 16:42:38 +0200

    clean

--- backend/src/test/java/afterady/security/auth/JwtUtilTest.java
+++ backend/src/test/java/afterady/security/auth/JwtUtilTest.java
@@ -2,27 +2,27 @@ package afterady.security.auth;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class JwtUtilTest {
+    @MockBean
+    private HttpServletRequest mockedRequest;
 
     @Autowired
     private JwtUtil jwtUtil;
 
-
     @Test
     public void shouldNotParseTokenWhenAuthHeaderIsMissing() {
         // arrange
-        HttpServletRequest mockedRequest = Mockito.mock(HttpServletRequest.class);
         when(mockedRequest.getHeader("Authorization")).thenReturn(null);
 
         // act
@@ -30,12 +30,12 @@ class JwtUtilTest {
 
         // assert
         assertTrue(token.isEmpty());
+        verifyGetAuthorizationHeaderFromRequest();
     }
 
     @Test
     public void shouldNotParseTokenWhenAuthHeaderDoesNotStartWithBearer() {
         // arrange
-        HttpServletRequest mockedRequest = Mockito.mock(HttpServletRequest.class);
         when(mockedRequest.getHeader("Authorization")).thenReturn("token 12345");
 
         // act
@@ -43,12 +43,12 @@ class JwtUtilTest {
 
         // assert
         assertTrue(token.isEmpty());
+        verifyGetAuthorizationHeaderFromRequest();
     }
 
     @Test
     public void shouldParseJwtToken() {
         // arrange
-        HttpServletRequest mockedRequest = Mockito.mock(HttpServletRequest.class);
         when(mockedRequest.getHeader("Authorization")).thenReturn("Bearer iKBIGBomyDoiFfOobySPWQsC7za0QKoEHaf15WVBP9rqt6SIQrETBXvkjpFaw2Bs");
 
         // act
@@ -57,5 +57,11 @@ class JwtUtilTest {
         // assert
         assertTrue(token.isPresent());
         assertEquals("iKBIGBomyDoiFfOobySPWQsC7za0QKoEHaf15WVBP9rqt6SIQrETBXvkjpFaw2Bs", token.get());
+        verifyGetAuthorizationHeaderFromRequest();
+    }
+
+    private void verifyGetAuthorizationHeaderFromRequest() {
+        verify(mockedRequest, times(1)).getHeader("Authorization");
+        verifyNoMoreInteractions(mockedRequest);
     }
 }
