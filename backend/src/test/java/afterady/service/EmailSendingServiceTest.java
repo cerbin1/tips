cb7f89483d3797fbf546b2888e9a9f895d4506ed
cerbin1 <gekarez@gmail.com>
2024-08-29 17:03:19 +0200

    Refactor EmailSendingServiceTest. Remove redundant @SpringBootTest

--- backend/src/test/java/afterady/service/EmailSendingServiceTest.java
+++ backend/src/test/java/afterady/service/EmailSendingServiceTest.java
@@ -3,33 +3,42 @@ package afterady.service;
 import afterady.config.EnvironmentWrapper;
 import afterady.service.email.EmailSendingService;
 import afterady.service.email.Sender;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static afterady.config.EnvironmentWrapper.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class EmailSendingServiceTest {
 
-    @Autowired
     private EmailSendingService emailSendingService;
 
-    @MockBean
+    @Mock
     private Sender sender;
 
-    @MockBean
+    @Mock
     private EnvironmentWrapper environment;
+
+    @BeforeEach
+    void init() {
+        emailSendingService = new EmailSendingService(sender, environment);
+    }
 
     @Test
     public void shouldSendEmail() {
         // arrange
         when(environment.getEnv(AFTERADY_MAIL_SENDER)).thenReturn("email");
         when(environment.getEnv(AFTERADY_MAIL_PWD)).thenReturn("password");
-        when(environment.getEnv(AFTERADY_MAIL_HOST)).thenReturn("host");
         when(sender.send("email", "password", "to", "subject", "content")).thenReturn(true);
 
         // act
@@ -37,5 +46,11 @@ public class EmailSendingServiceTest {
 
         // assert
         assertTrue(emailSent);
+        verify(sender, times(1)).send("email", "password", "to", "subject", "content");
+        verifyNoMoreInteractions(sender);
+        verify(environment, times(1)).getEnv(AFTERADY_MAIL_SENDER);
+        verify(environment, times(1)).getEnv(AFTERADY_MAIL_PWD);
+//        verify(environment, times(1)).getEnv(AFTERADY_MAIL_HOST);
+        Mockito.verifyNoMoreInteractions(environment);
     }
 }
