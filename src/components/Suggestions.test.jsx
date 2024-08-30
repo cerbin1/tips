5992c43768cad7cd334cc4548ee8381178a2ebde
cerbin1 <gekarez@gmail.com>
2024-08-30 18:40:11 +0200

    Refactor Suggestions.test

--- src/components/Suggestions.test.jsx
+++ src/components/Suggestions.test.jsx
@@ -2,24 +2,22 @@ import { renderWithAuth } from "../test/test-utils";
 import Suggestions from "./Suggestions";
 
 describe("Suggestions", () => {
-  test("should display button to change suggestion type", () => {
+  test("should render component", () => {
     renderWithAuth(<Suggestions />);
 
     expect(screen.getByTestId("suggestions-section")).toBeInTheDocument();
-    expect(
-      screen.getByText("Przejdź do propozycji kategorii")
-    ).toBeInTheDocument();
-  });
-
-  test("should display advice suggestion on component render", () => {
-    renderWithAuth(<Suggestions />);
-
+    const changeSuggestionTypeButton = screen.getByText(
+      "Przejdź do propozycji kategorii"
+    );
+    expect(changeSuggestionTypeButton).toBeInTheDocument();
     expect(screen.getByText("Zaproponuj poradę")).toBeInTheDocument();
   });
 
-  test("should change to category suggestion on button click", () => {
+  test("should change to category suggestion on button click", async () => {
     renderWithAuth(<Suggestions />);
 
-    userEvent.click(screen.getByText("Przejdź do propozycji kategorii"));
+    await userEvent.click(screen.getByText("Przejdź do propozycji kategorii"));
+
+    expect(screen.getByText("Zaproponuj kategorię")).toBeInTheDocument();
   });
 });
