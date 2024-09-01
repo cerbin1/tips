a1c10f7b5089e89cb620781a4e6b879b887789f2
cerbin1 <gekarez@gmail.com>
2024-09-01 14:25:58 +0200

    Improve SecondaryButton.test

--- src/components/common/SecondaryButton.test.jsx
+++ src/components/common/SecondaryButton.test.jsx
@@ -2,9 +2,9 @@ import SecondaryButton from "./SecondaryButton";
 
 describe("SecondaryButton", () => {
   test("should render component", () => {
-    render(<SecondaryButton onClick={() => {}}>test</SecondaryButton>);
+    render(<SecondaryButton onClick={() => {}}>Button</SecondaryButton>);
 
-    const button = screen.getByText("test");
+    const button = screen.getByText("Button");
     expect(button).toBeInTheDocument();
     expect(button).toHaveClass(
       "px-6 py-3 bg-slate-400 text-white text-lg rounded hover:bg-slate-500 transition-colors duration-300"
